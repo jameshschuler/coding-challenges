b810f2e41266d7cf32ce992c668d7dabfdcec33c
James Schuler <jameshschuler1@gmail.com>
2021-04-06 22:04:46 -0400

    added skeleton for second challenge

---
+++ src/02/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Given an integer n and a sorted array of prime integers called primes, return the nth “super ugly number”. 
+ * A “super ugly number” is a positive number whose all prime factors are in the array primes. 
+ * 
+ * Example:
+ * 
+ * superUgly(1, [2,3,5])
+ * 1
+ * superUgly(11, [2,7,13,19])
+ * 28
+ * 
+ */
+
+function superUgly ( n: number, primes: number[] ) {
+
+}
