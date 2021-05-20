760b6ddc73c49e1e18c98e654f927fef6c3666cd
James Schuler <jameshschuler1@gmail.com>
2021-05-19 22:24:24 -0400

    added week 03 files

---
+++ src/03/index.ts
@@ -0,0 +1,12 @@
+/**
+ *
+ * Given an array of 0s and 1s that represent a garden, where 0 is a plot that hasn’t been planted on,
+ * and 1 is a plot that has been planted on, return true if n plants can be planted without touching another plant.
+ *
+ * garden = [1,0,0,0,1]
+ * canPlant(garden, 1)
+ * true // plant at position 2
+ * canPlant(garden, 4)
+ * false // there are only 3 plots, and two of them can't be planted on
+ *
+ */
